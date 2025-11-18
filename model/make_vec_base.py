403a63e0844d03215cbe0696b7dd0c2f13fa3153
lluuww0112 <tgur0620@gmail.com>
2025-11-19 01:17:02 +0900

    make_vec_base.py updated

--- model/make_vec_base.py
+++ model/make_vec_base.py
@@ -1,53 +1,69 @@
 import os
 
 import torch
-from torch.utils.data import DataLoader
 
 from dataload import DiaryLyricData
-from model import LyricEncder
+from model import LyricEncder, DiaryEncoder
 
 import pickle
 
-# Load Model
+# get saved model path
 _base_dir = os.path.dirname(__file__)
-_lyric_weight_path = os.path.join(_base_dir, "model_save", "lyricEncoder.pth")
+_lyric_weight_path = os.path.join(_base_dir, "model_save", "Cross_lyricEncoder.pth")
+_diary_weight_path = os.path.join(_base_dir, "model_save", "Cross_diaryEncoder.pth")
 
-model = LyricEncder()
-model = torch.load(_lyric_weight_path, weights_only=False)
+# load model
+lyricEncoder = LyricEncder()
+lyricEncoder = torch.load(_lyric_weight_path, weights_only=False)
 
-# set Data
-dataloader = DataLoader(
-    dataset=DiaryLyricData,
-    batch_size=64,
-    shuffle=False,
-)
+diaryEncoder = DiaryEncoder()
+diaryEncoder = torch.load(_diary_weight_path, weights_only=False)
+
+# =========================================================================
 
 # Lyric Encoding
 device = "cuda" if torch.cuda.is_available() else "cpu"
-result = []
-model.to(device)
-model.eval()
-
-# for batch in dataloader:
-#     x = batch["lyric"]
-#     with torch.no_grad():
-#         encoded = model.encode(x)    # shape: (batch, emb_dim)
-    
-#     encoded = encoded.to("cpu")      # GPU 텐서는 CPU로 이동
-#     result.append(encoded)
+lyric_result = []
+lyricEncoder.to(device)
+lyricEncoder.eval()
 
 for idx in range(len(DiaryLyricData)):
     lyric = DiaryLyricData[idx]["lyric"]
     with torch.no_grad():
-        encoded = model.encode(lyric)
+        encoded = lyricEncoder.encode(lyric)
     encoded = encoded.to("cpu")
-    result.append(encoded)
+    lyric_result.append(encoded)
 
-# (sample_num, emb_dim) 형태로 합치기
-result_tensor = torch.cat(result, dim=0) 
 
 # save lyricVector
 with open("./vec_index.pkl", "wb") as f:
-    pickle.dump(result_tensor.tolist() , f)
+    lyric_result_tensor = torch.cat(lyric_result, dim=0) 
+    pickle.dump(lyric_result_tensor.tolist() , f)
 
-print("lyric encoded vector 저장 완료", result_tensor.shape)
+print("lyric encoded vector 저장 완료", lyric_result_tensor.shape)
+
+# =========================================================================
+
+# Diary Encoding
+device = "cuda" if torch.cuda.is_available() else "cpu"
+diary_result = []
+diaryEncoder.to(device)
+diaryEncoder.eval()
+
+
+for idx in range(len(DiaryLyricData)):
+    diary = DiaryLyricData[idx]["diary"]
+    with torch.no_grad():
+        _, encoded = diaryEncoder.encode(diary)
+
+    encoded = encoded.to("cpu").unsqueeze(0)
+    diary_result.append(encoded)
+
+# save diaryVector
+with open("./vec_index_diary.pkl", "wb") as f:
+    diary_result_tensor = torch.cat(diary_result, dim=0) 
+    pickle.dump(diary_result_tensor.tolist() , f)
+
+
+print("diary encoded vector 저장 완료", diary_result_tensor.shape)
+
